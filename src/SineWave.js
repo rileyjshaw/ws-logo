07f7e46114f220dd9dda2c71c8cfa811a09cdcfe
Riley Shaw <rileyjshaw@gmail.com>
2024-05-31 12:24:11 -0400

    Change SineWave from meshline to Drei’s Line

--- src/SineWave.js
+++ src/SineWave.js
@@ -1,46 +1,29 @@
 import React, { useRef, useMemo } from 'react';
-import { useFrame, extend, useThree } from '@react-three/fiber';
+import { useFrame } from '@react-three/fiber';
+import { Line } from '@react-three/drei';
 // import { useMask } from '@react-three/drei';
-import { Vector3 } from 'three';
-import { MeshLineGeometry, MeshLineMaterial, raycast as MeshLineRaycast } from 'meshline';
-
-extend({ MeshLineGeometry, MeshLineMaterial });
+import { X, Y } from './constants';
 
 const SineWave = ({ segments = 100, color }) => {
 	// const stencil = useMask(1);
-	const { size } = useThree();
-	const { width, height } = size;
 	const curveRef = useRef();
 
 	const points = useMemo(() => {
 		return new Array(segments).fill().map((_, i) => {
-			const x = ((i / (segments - 1)) * 2 - 1) * 0.99;
+			const x = ((i / (segments - 1)) * 2 - 1) * 0.95;
 			const y = Math.sin(-x * Math.PI) / 3;
-			return new Vector3(x, y, 0);
+			return [x, y, 0];
 		});
 	}, [segments]);
 
 	useFrame(({ clock }) => {
 		points.forEach(point => {
-			point.y = Math.sin((clock.getElapsedTime() / 2 - point.x) * Math.PI) / 3;
+			point[Y] = Math.sin((clock.getElapsedTime() / 2 - point[X]) * Math.PI) / 3;
 		});
-		curveRef.current.geometry.setPoints(points);
+		curveRef.current.geometry.setPositions(points.flat());
 	});
 
-	return (
-		<mesh raycast={MeshLineRaycast} ref={curveRef} dispose={null}>
-			<meshLineGeometry attach="geometry" points={points} />
-			<meshLineMaterial
-				attach="material"
-				depthTest={false}
-				lineWidth={0.25 - Math.max(width, height) / 27500}
-				color={color}
-				sizeAttenuation={true}
-				transparent
-				// {...stencil}
-			/>
-		</mesh>
-	);
+	return <Line ref={curveRef} points={points} color={color} lineWidth={17} depthTest={false} />;
 };
 
 export default SineWave;
