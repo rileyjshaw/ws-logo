2bae20a64d7b84680e5b2e9fa2310f3054ca5c2c
Riley Shaw <rileyjshaw@gmail.com>
2024-06-25 23:51:35 -0700

    Logo proposal v1

--- src/App.js
+++ src/App.js
@@ -1,58 +1,22 @@
-import { useMemo } from 'react';
-import { Canvas, useThree, useFrame } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import { Center } from '@react-three/drei';
 import SpinningRing from './SpinningRing';
 import SineWave from './SineWave';
-import TextCircle from './TextCircle';
 import AlwaysFacing from './AlwaysFacing';
 import Mask from './Mask';
-import Text from './Text';
-import useCountdown from './hooks';
 import { WATERSHED_BLUE } from './constants';
 
-const N_RINGS = 5;
-
-const rings = Array.from({ length: N_RINGS }, (_, index) => ({
+const rings = [-30, 0, 30].map((deg, index) => ({
 	position: [0, 0, 0],
-	rotation: [0, (Math.PI / N_RINGS) * index, 0],
+	rotation: [0, (deg / 180) * Math.PI + Math.PI / 2, 0],
 	color: WATERSHED_BLUE,
 }));
 
-function Rig() {
-	const { camera } = useThree();
-	const positionVec = useMemo(() => ({ x: 0, y: 0 }), []);
-	const rotationVec = useMemo(() => ({ x: 0, y: 0 }), []);
-	return useFrame(({ mouse }) => {
-		positionVec.x = mouse.x * 2;
-		positionVec.y = mouse.y * 2;
-		positionVec.z = camera.position.z;
-		camera.position.lerp(positionVec, 0.02);
-
-		rotationVec.x = -mouse.y * Math.PI;
-		rotationVec.y = -mouse.x * Math.PI;
-		camera.rotation.setFromVector3(rotationVec);
-
-		camera.lookAt(0, 0, 0);
-	});
-}
-
 const App = () => {
-	const daysLeft = useCountdown(2024, 6, 18);
-
 	return (
-		<Canvas flat camera={{ position: [0, 0, 8] }} gl={{ antialias: true }}>
+		<Canvas flat dpr={4} camera={{ position: [0, 0, 8] }} gl={{ antialias: true }}>
 			<color attach="background" args={['#fff']} />
 			<Center position={[0, 0, 0]}>
-				<Center position={[0, 2.5, 0.5]}>
-					<Text size={0.5} font={`${process.env.PUBLIC_URL}/easy_grotesk.json`}>
-						Hi team,
-					</Text>
-				</Center>
-				<TextCircle
-					text="I’m so excited to work together!  "
-					radius={1.2}
-					font={`${process.env.PUBLIC_URL}/cartograph.json`}
-				/>
 				<AlwaysFacing>
 					<Mask color={WATERSHED_BLUE} />
 				</AlwaysFacing>
@@ -60,18 +24,7 @@ const App = () => {
 				{rings.map((props, index) => (
 					<SpinningRing key={index} {...props} />
 				))}
-				<Center position={[0, -2.5, 0.5]}>
-					<Text size={0.5} font={`${process.env.PUBLIC_URL}/easy_grotesk.json`}>
-						{daysLeft > 0 ? 'See you in' : 'Let’s decarbonize'}
-					</Text>
-				</Center>
-				<Center position={[0, -3.5, 0.5]}>
-					<Text size={0.5} font={`${process.env.PUBLIC_URL}/easy_grotesk.json`}>
-						{daysLeft > 0 ? `${daysLeft} days!` : 'the economy!'}
-					</Text>
-				</Center>
 			</Center>
-			<Rig />
 		</Canvas>
 	);
 };
--- src/SineWave.js
+++ src/SineWave.js
@@ -1,30 +1,29 @@
 import React, { useRef, useMemo } from 'react';
-import { useFrame, useThree } from '@react-three/fiber';
+import { useThree } from '@react-three/fiber';
 import { Line } from '@react-three/drei';
-// import { useMask } from '@react-three/drei';
-import { X, Y } from './constants';
 
 const SineWave = ({ segments = 100, color }) => {
-	// const stencil = useMask(1);
 	const { size } = useThree();
 	const curveRef = useRef();
 
 	const points = useMemo(() => {
 		return new Array(segments).fill().map((_, i) => {
-			const x = ((i / (segments - 1)) * 2 - 1) * 0.95;
+			const x = (i / (segments - 1)) * 2 - 1;
 			const y = Math.sin(-x * Math.PI) / 3;
 			return [x, y, 0];
 		});
 	}, [segments]);
 
-	useFrame(({ clock }) => {
-		points.forEach(point => {
-			point[Y] = Math.sin((clock.getElapsedTime() / 2 - point[X]) * Math.PI) / 3;
-		});
-		curveRef.current.geometry.setPositions(points.flat());
-	});
-
-	return <Line ref={curveRef} points={points} color={color} lineWidth={2 + size.height / 80} depthTest={false} />;
+	return (
+		<Line
+			rotation={[0, 0, 0]}
+			ref={curveRef}
+			points={points}
+			color={color}
+			lineWidth={2 + size.height / 80}
+			depthTest={false}
+		/>
+	);
 };
 
 export default SineWave;
